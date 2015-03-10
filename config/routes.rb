aedafea0996a6b1312f6b718c24c3557a9338bce
ebarra <ebarra@dit.upm.es>
2015-03-10 14:22:03 +0100

    legal notice, conditions, cookies, so on

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,8 @@ Vish::Application.routes.draw do
   match 'faq' => 'static#overview'
   match 'help' => 'static#overview'
   match 'legal_notice' => 'static#legal_notice'
+  match 'privacy_policy' => 'static#privacy_policy'
+  match 'conditions_of_use' => 'static#conditions_of_use'
   #Download the user manual and count the number of downloads
   match 'user_manual' => 'static#download_user_manual'
   
