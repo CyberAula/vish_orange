783c2d7b8dccf8eb71e8a2805923019e17f700b5
Lourdesmarco <lourdesmarco.design@gmail.com>
2015-07-23 13:17:31 +0200

    Overview changes

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,7 +4,7 @@ class StaticController < ApplicationController
   def download_user_manual
     #add one to the user manual download count
     Stats.increment("user_manual_download_count", 1)
-    send_file "#{Rails.root}/public/vish_user_manual.pdf", :type => 'application/pdf'
+    send_file "#{Rails.root}/public/EducaInternet.pdf", :type => 'application/pdf'
   end
 
   def legal_notice
