b5c253b8c4104d53642d6b500d0e7311a95f83d9
abenitoc <vermibeto@gmail.com>
2015-09-15 13:46:01 +0200

    Added Mailers and notify teacher works after uploads

--- app/controllers/excursions_controller.rb
+++ app/controllers/excursions_controller.rb
@@ -8,7 +8,7 @@ class ExcursionsController < ApplicationController
   skip_load_and_authorize_resource :only => [ :excursion_thumbnails, :metadata, :scormMetadata, :iframe_api, :preview, :clone, :manifest, :evaluate, :last_slide, :downloadTmpJSON, :uploadTmpJSON, :interactions, :upload_attachment, :show_attachment]
   skip_before_filter :store_location, :if => :format_full?
   skip_after_filter :discard_flash, :only => [:clone]
-  #after_filter :notify_teacher, :only => [:create, :update]
+  after_filter :notify_teacher, :only => [:create, :update]
   
   # Enable CORS
   before_filter :cors_preflight_check, :only => [:excursion_thumbnails,:last_slide,:iframe_api]
@@ -492,18 +492,14 @@ class ExcursionsController < ApplicationController
       file_new_name
   end
 
-  =begin
   def notify_teacher
-    binding.pry
-      unless params[:excursion].nil?
-        author = Excursion.find(params[:id]).user
-        unless author.private_student_group_id.nil?
-          binding.pry
-          teacher = Actor.find(PrivateStudentGroup.find(author.private_student_group_id).owner_id)
-          mail(:to => teacher.email, :subject => "Uploaded file")
-        
+      author_id = params[:excursion][:author_id] || JSON.parse(params[:excursion][:json])["author"]["vishMetadata"]["id"].to_i
+      unless author_id.nil?
+        author = Actor.find(author_id)
+        unless author.user.private_student_group_id.nil?
+          teacher = Actor.find(author.user.private_student_group.owner_id).user
+          #TeacherNotificationMailer.notify_teacher(teacher, "Subject", "Body", author_id)
         end
       end
   end
-  =end
 end
