fee98461d4a2ebe3ddd66a12a6f30450215c6cc2
ebarra <ebarra@dit.upm.es>
2015-10-09 12:18:57 +0200

    default locale to :es

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module Vish
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :es
     config.i18n.fallbacks = true
     
     # Configure the default encoding used in templates for Ruby 1.9.
