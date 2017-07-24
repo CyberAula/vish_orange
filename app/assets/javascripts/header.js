f404006b30d25e80c0fe812e7c6a607b4d089c83
damalga <dam.al.ga@gmx.com>
2017-07-24 16:59:30 +0200

    adjust header in all pages

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,10 +1,17 @@
-
-$( window ).scroll(function() {
-  var sh = $(window).scrollTop();
-	if (sh > 410) {
-    $("#navbar-home").css("background-color", "white");
+$(window).scroll(function(){
+var sh = $(window).scrollTop();
+	if(sh > 410){
+    $("#navbar-home").css({
+			"background-color": "#fff",
+			"box-shadow": "0 0 8px 0 rgba(0,0,0,0.2)"
+		});
+    $("#brand a").css("color", "#333");
 	}
-  if (sh < 410) {
-		$("#navbar-home").css("background-color", "transparent");
+  if(sh < 410){
+		$("#navbar-home").css({
+			"background-color": "transparent",
+			"box-shadow": "none"
+		});
+    $("#brand a").css("color", "#fff");
 	}
 });
