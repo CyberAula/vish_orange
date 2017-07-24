e98500c6dc09b6cd08de3dd8640331bad36658e4
damalga <dam.al.ga@gmx.com>
2017-07-24 16:31:41 +0200

    add explore route

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Vish::Application.routes.draw do
   match 'teach' => 'static#teach'
   match 'learn' => 'static#learn'
   match 'about' => 'static#about_us'
+  match 'explore' => 'static#explore'
   match 'train_yourself' => 'static#train_yourself'
   match 'train_students' => 'static#train_students'
   match 'download_perm_request' => 'static#download_perm_request'
