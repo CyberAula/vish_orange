964f1abec07f95db2cd9ae69cb376abfb651b3bc
Lourdesmarco <lourdesmarco.design@gmail.com>
2017-09-12 19:04:35 +0200

    contest structure & styles

--- app/assets/javascripts/scrollup.js
+++ app/assets/javascripts/scrollup.js
@@ -5,7 +5,7 @@ $(window).scroll(function(){
       $('.scrollup').fadeOut();
    }
   });
-  $('.scrollup').click(function(){
+  $('.scrollup, .spread').click(function(){
     $("html, body").animate({ scrollTop: 0 }, 600);
     return false;
   });
--- app/helpers/contest_helper.rb
+++ app/helpers/contest_helper.rb
@@ -21,6 +21,6 @@ module ContestHelper
 	end
 
 	def contest_fase
-		return 1
+		return 4
 	end
 end
