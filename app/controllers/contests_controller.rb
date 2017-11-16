20ef1b9c18218442337dbaf1df31ef5d899f420f
ebarra <ebarra@dit.upm.es>
2017-11-16 10:42:06 +0100

    send email removed

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -188,7 +188,8 @@ class ContestsController < ApplicationController
     @user = User.create(params[:user])
     if @user.save
       #create enrrollment
-      CourseNotificationMailer.user_welcome_email(@user, @contest)
+      #TODO prepare email for registered users in the contest
+      #CourseNotificationMailer.user_welcome_email(@user, @contest)
       if @contest.has_additional_fields?
         additional_fields =  {}
         @contest.additional_fields.map do |n|
