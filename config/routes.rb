02a64738e35386a9181c2ab47beda7d30b5104de
aemartos <aemartos@gmail.com>
2017-11-17 12:09:22 +0100

    Mail 2nd contest EducaInternet.

--- config/routes.rb
+++ config/routes.rb
@@ -55,7 +55,8 @@ Vish::Application.routes.draw do
   match 'educatip1' => 'static#educatip1'
   match 'educatip2' => 'static#educatip2'
   match 'educatip3' => 'static#educatip3'
-  match 'email_test' => 'static#email_test'
+  match 'email1' => 'static#contest_welcome_email_test'
+  match 'email2' => 'static#platform_welcome_email_test'
 
   #APIs
   match '/apis/search' => 'federated_search#search'
