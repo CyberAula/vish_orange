78d6f18ed42e594d2186274dc67df5b03df32076
aemartos <aemartos@gmail.com>
2018-01-29 13:20:21 +0100

    images for courses

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,7 +1,11 @@
 module CoursesHelper
   
   def course_thumb_for(course)
-    image_tag (course.thumbnail_url)
+    if(course.getAvatarUrl.nil?)
+      return image_tag "/assets/logos/original/default_course.png"
+    else
+      return image_tag Vish::Application.config.full_domain + course.avatar.url(:original)
+    end
   end
 
   def course_raw_thumbail(course)
