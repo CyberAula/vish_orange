004254f42b6d9f34744566d2047347d209746cbd
aemartos <aemartos@gmail.com>
2018-06-06 17:25:44 +0200

    Media preen set, icon explanations added in lateral menu

--- app/assets/javascripts/elab.js
+++ app/assets/javascripts/elab.js
@@ -273,28 +273,28 @@ function debounce( fn, threshold ) {
 // -------- REMOVE HOVER ONTOUCH -----------
  
 
-function hasTouch() {
+  function hasTouch() {
     return 'ontouchstart' in document.documentElement
-           || navigator.maxTouchPoints > 0
-           || navigator.msMaxTouchPoints > 0;
-}
+      || navigator.maxTouchPoints > 0
+      || navigator.msMaxTouchPoints > 0;
+  }
 
-if (hasTouch()) { // remove all :hover stylesheets
+  if (hasTouch()) { // remove all :hover stylesheets
     try { // prevent exception on browsers not supporting DOM styleSheets properly
-        for (var si in document.styleSheets) {
-            var styleSheet = document.styleSheets[si];
-            if (!styleSheet.rules) continue;
+      for (var si in document.styleSheets) {
+        var styleSheet = document.styleSheets[si];
+        if (!styleSheet.rules) continue;
 
-            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
-                if (!styleSheet.rules[ri].selectorText) continue;
+        for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+          if (!styleSheet.rules[ri].selectorText) continue;
 
-                if (styleSheet.rules[ri].selectorText.match(':hover')) {
-                    styleSheet.deleteRule(ri);
-                }
-            }
+          if (styleSheet.rules[ri].selectorText.match(':hover')) {
+              styleSheet.deleteRule(ri);
+          }
         }
+      }
     } catch (ex) {}
-}
+  }
 
 
 
