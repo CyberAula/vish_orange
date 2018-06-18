1765c028a1a911533832ed9e15a2f8830b48dc0d
ebarra <ebarra@dit.upm.es>
2018-06-18 13:15:52 +0200

    translations for elab

---
+++ app/assets/javascripts/elab_apps.js
@@ -0,0 +1,501 @@
+//-------- APPS JSON -----------
+
+  // definition 180 characters maximun, because of the responsive
+  var OFICIAL = [
+    {
+    name:"fakedetector health",
+    app: "fake detector",
+    class: "fakedetector",
+    type: "health",
+    category: "fake",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "30min",
+    difficulty: "fácil",
+    definition: "aprende a detectar noticias falsas en la red con nuestro fakedetector. no te dejes engañar, no todo lo que aparece en internet es cierto. ¡que no te den gato por liebre!",
+    url: "#"
+    },
+   {
+    name:"wordwall internet1",
+    app: "word wall",
+    class: "wordwall",
+    type: "internet1",
+    category: "definitions",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "1hora",
+    difficulty: "media",
+    definition: "completa el muro de 25 letras con definiciones sobre las TIC e internet. te damos como pista si empieza o contiene dicha letra, tienes comodines para ayudarte, ¡úsalos bien!",
+    url: "#"
+  },
+  {
+    name:"passcheck 1",
+    app: "pass check",
+    class: "passcheck",
+    type: "1",
+    category: "security",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "10min",
+    difficulty: "fácil",
+    definition: "aprende a crear contraseñas seguras con passcheck. para superar este reto tendrás que comprobar 3 contraseñas: 1 fácil, 1 media y 1 segura, con ayuda de nuestra aplicación.",
+    url: "#"
+  },
+  {
+    name:"fakedetector phishing",
+    app: "fake detector",
+    class: "fakedetector",
+    type: "phishing",
+    category: "fake",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "30min",
+    difficulty: "fácil",
+    definition: "aprende a detectar phishing en la red con nuestro fakedetector. no te dejes engañar, no todo lo que aparece en internet es cierto. ¡que no te den gato por liebre!",
+    url: "#"
+  },
+  {
+    name:"wordwall internet2",
+    app: "word wall",
+    class: "wordwall",
+    type: "internet2",
+    category: "definitions",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "1hora",
+    difficulty: "difícil",
+    definition: "completa el muro de 25 letras con definiciones sobre las TIC e internet. te damos como pista si empieza o contiene dicha letra, tienes comodines para ayudarte, ¡úsalos bien!",
+    url: "#"
+  },
+  {
+    name:"passcheck 2",
+    app: "pass check",
+    class: "passcheck",
+    type: "2",
+    category: "security",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "10min",
+    difficulty: "fácil",
+    definition: "aprende a crear contraseñas seguras con passcheck. para superar este reto tendrás que comprobar 3 contraseñas seguras, con ayuda de nuestra aplicación.",
+    url: "#"
+  },
+  {
+    name:"quizgenerator internet",
+    app: "quiz generator",
+    class: "quizgenerator",
+    type: "tech",
+    category: "quiz",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "20min",
+    difficulty: "fácil",
+    definition: "¡estamos trabajando en ello! próximamente podrás entrenarte en diferentes materias con breves quizes de preguntas de respuesta múltiple, para ser el empo de la clase.",
+    url: "#"
+  }
+  ];
+
+  var SAMPLE = [
+  {
+    name:"fakedetector news",
+    app: "fake detector",
+    class: "fakedetector",
+    type: "news",
+    category: "fake",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "30min",
+    difficulty: "fácil",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    url: "#"
+  },
+   {
+    name:"wordwall internet",
+    app: "word wall",
+    class: "wordwall",
+    type: "internet",
+    category: "definitions",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "1hora",
+    difficulty: "fácil",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    url: "#"
+  },
+  {
+    name:"passcheck 1",
+    app: "pass check",
+    class: "passcheck",
+    type: "1",
+    category: "security",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "10min",
+    difficulty: "fácil",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    url: "#"
+  },
+  {
+    name:"fakedetector health",
+    app: "fake detector",
+    class: "fakedetector",
+    type: "health",
+    category: "fake",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "30min",
+    difficulty: "fácil",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    url: "#"
+  },
+  {
+    name:"passcheck 2",
+    app: "pass check",
+    class: "passcheck",
+    type: "2",
+    category: "security",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "10min",
+    difficulty: "fácil",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    url: "#"
+  },
+  {
+    name:"quizgenerator internet",
+    app: "quiz generator",
+    class: "quizgenerator",
+    type: "internet",
+    category: "quiz",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "20min",
+    difficulty: "fácil",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    url: "#"
+  },
+  {
+    name:"fakedetector phishing",
+    app: "fake detector",
+    class: "fakedetector",
+    type: "phishing",
+    category: "fake",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "30min",
+    difficulty: "fácil",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    url: "#"
+  },
+   {
+    name:"passcheck 3",
+    app: "pass check",
+    class: "passcheck",
+    type: "3",
+    category: "security",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "10min",
+    difficulty: "fácil",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    url: "#"
+  },
+  {
+    name:"quizgenerator tic",
+    app: "quiz generator",
+    class: "quizgenerator",
+    type: "tic",
+    category: "quiz",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "20min",
+    difficulty: "fácil",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    url: "#"
+  },
+  {
+    name:"wordwall tic",
+    app: "word wall",
+    class: "wordwall",
+    type: "tic",
+    category: "definitions",
+    author: "elab",
+    language: "español",
+    date: "20|03|18",
+    rate: "TP",
+    duration: "1hora",
+    difficulty: "fácil",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    url: "#"
+  }
+  ];
+
+    var SAMPLE_LARGE = [
+  {
+    name:"fake detector news",
+    app: "fake detector",
+    class: "fakedetector",
+    type: "news",
+    category: "fake",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+   {
+    name:"wordwall internet",
+    app: "word wall",
+    class: "wordwall",
+    type: "internet",
+    category: "definitions",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"pass check 1",
+    app: "pass check",
+    class: "passcheck",
+    type: "1",
+    category: "security",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"fake detector health",
+    app: "fake detector",
+    class: "fakedetector",
+    type: "health",
+    category: "fake",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"pass check 2",
+    app: "pass check",
+    class: "passcheck",
+    type: "2",
+    category: "security",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"quiz generator internet",
+    app: "quiz generator",
+    class: "quizgenerator",
+    type: "internet",
+    category: "quiz",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"fake detector phishing",
+    app: "fake detector",
+    class: "fakedetector",
+    type: "phishing",
+    category: "fake",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+   {
+    name:"pass check 3",
+    app: "pass check",
+    class: "passcheck",
+    type: "3",
+    category: "security",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"quiz generator tic",
+    app: "quiz generator",
+    class: "quizgenerator",
+    type: "tic",
+    category: "quiz",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"wordwall tic",
+    app: "word wall",
+    class: "wordwall",
+    type: "tic",
+    category: "definitions",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"pass check 3",
+    app: "pass check",
+    class: "passcheck",
+    type: "3",
+    category: "security",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"quiz generator tic",
+    app: "quiz generator",
+    class: "quizgenerator",
+    type: "tic",
+    category: "quiz",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"wordwall tic",
+    app: "word wall",
+    class: "wordwall",
+    type: "tic",
+    category: "definitions",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  }
+
+  ];
+
+  var SAMPLE_ALL = [
+  {
+    name:"fake detector news",
+    app: "fake detector",
+    class: "fakedetector",
+    type: "news",
+    category: "fake",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"fake detector phishing",
+    app: "fake detector",
+    class: "fakedetector",
+    type: "phishing",
+    category: "fake",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"fake detector health",
+    app: "fake detector",
+    class: "fakedetector",
+    type: "health",
+    category: "fake",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"pass check 1",
+    app: "pass check",
+    class: "passcheck",
+    type: "1",
+    category: "security",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"pass check 2",
+    app: "pass check",
+    class: "passcheck",
+    type: "2",
+    category: "security",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"pass check 3",
+    app: "pass check",
+    class: "passcheck",
+    type: "3",
+    category: "security",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"quiz generator internet",
+    app: "quiz generator",
+    class: "quizgenerator",
+    type: "internet",
+    category: "quiz",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"quiz generator tic",
+    app: "quiz generator",
+    class: "quizgenerator",
+    type: "tic",
+    category: "quiz",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"wordwall internet",
+    app: "word wall",
+    class: "wordwall",
+    type: "internet",
+    category: "definitions",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  },
+  {
+    name:"wordwall tic",
+    app: "word wall",
+    class: "wordwall",
+    type: "tic",
+    category: "definitions",
+    definition: "lorem fistrum diodeno pecador me cago en tus muelas se calle ustée papaar papaar ese que llega papaar papaar te voy a borrar el cerito. ahorarr a gramenawer sexuarl a peich pupita.",
+    difficulty: "easy",
+    url: "#"
+  }
+
+  ];
